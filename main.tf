4cbad252a48ab01c6aa7cd61451b40174dad4ce8
Luis Medinelli <luis.medinelli@gmail.com>
2020-10-30 02:21:54 +0000

    Added the Billable tag

--- main.tf
+++ main.tf
@@ -126,6 +126,7 @@ resource "aws_instance" "hashicat" {
   tags = {
     Name = "${var.prefix}-hashicat-instance"
     Department = "devops"
+    Billable = "true"
   }
 }
 # We're using a little trick here so we can run the provisioner without
